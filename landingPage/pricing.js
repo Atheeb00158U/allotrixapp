6733e6d6e51effc11b1b04cc30fbfa4d5e402f97
Mohamed Aashir <tmohamedaashir@gmail.com>
2023-12-03 23:42:13 +0530

    updated pricing

--- landingPage/pricing.js
+++ landingPage/pricing.js
@@ -30,16 +30,12 @@ export function pricing(){
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
           <p>Upto 25 Smart Allotments</p>
         </article>
-        <article>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
-          <p>1 Account Access</p>
-        </article>
       </div>
     </article>
 
     <!-- Item 2 -->
     <article class="subs-item" style="background-color: #AA362F;">
-      <h3 class="sub-head">Standard</h3>
+      <h3 class="sub-head">Automation Starter</h3>
       <h2>Rs. 7000</h2>
       <p style="font-size: 13px; font-family: 'Poppins', sans-serif; font-weight: 200; margin-top: -8px;">( for <strong>1</strong> MUN )</p>
       <div class="sub-data">
@@ -53,10 +49,6 @@ export function pricing(){
         </article>
         <article>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
-          <p>Custom Branding</p>
-        </article>
-        <article>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
           <p>Automatic Emails</p>
         </article>
         <article>
@@ -67,16 +59,12 @@ export function pricing(){
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
           <p>24/7 Customer Support</p>
         </article>
-        <article>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
-          <p>2 Accounts Access</p>
-        </article>
       </div>
     </article>
 
     <!-- Item 3 -->
     <article class="subs-item" style="background-color: #AA362F;">
-      <h3 class="sub-head">Premium</h3>
+      <h3 class="sub-head">Automation Premium</h3>
       <h2>Rs. 10000</h2>
       <p style="font-size: 13px; font-family: 'Poppins', sans-serif; font-weight: 200; margin-top: -8px;">( for <strong>2</strong> MUN )</p>
       <div class="sub-data">
@@ -104,16 +92,12 @@ export function pricing(){
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
           <p>24/7 Customer Support</p>
         </article>
-        <article>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
-          <p>2 Accounts Access</p>
-        </article>
       </div>
     </article>
 
     <!-- Item 3 -->
     <article class="subs-item" style="background-color: #AA362F;">
-      <h3 class="sub-head">Enterprise</h3>
+      <h3 class="sub-head">Automation Enterprise</h3>
       <h2>Rs. 25000</h2>
       <p style="font-size: 13px; font-family: 'Poppins', sans-serif; font-weight: 200; margin-top: -8px;">( for <strong>10</strong> years )</p>
       <div class="sub-data">
@@ -140,10 +124,6 @@ export function pricing(){
         <article>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
           <p>24/7 Customer Support</p>
-        </article>
-        <article>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>
-          <p>2 Accounts Access</p>
         </article>
       </div>
     </article>
