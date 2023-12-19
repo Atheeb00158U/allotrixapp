8cb72777aaa1e1c0603410ff6a7ee21e069c75e5
Mohamed Aashir <tmohamedaashir@gmail.com>
2023-12-19 21:45:52 +0530

    fix: updates

--- updates.js
+++ updates.js
@@ -2,32 +2,32 @@ const workingContainer = document.querySelector('#working-container');
 
 const updates = [
   {
-    "date": "2023-01-20",
+    "date": "2024-01-20",
     "title": "Double Delegation Allotment",
     "body": "Double the diplomacy, double the challenges, and double the fun. Allotrix is integrating double delegation allotments feature soon!"
   },
   {
-    "date": "2023-02-14",
+    "date": "2024-02-14",
     "title": "AI-Powered Committee Allotment",
     "body": "Introducing cutting-edge AI integration! Simply provide the agenda and committee size, and our advanced algorithm will craft a meticulously curated list of countries, complete with a pertinency split. Experience the power of intelligent committee composition effortlessly."
   },
   {
-    "date": "2023-02-15",
+    "date": "2024-02-15",
     "title": "Effortless Reallotment",
     "body": "Say goodbye to hours of navigation and hard work! With just a few clicks, reallotment becomes a breeze. Enjoy the convenience of swift and efficient changes, ensuring your MUN logistics are in perfect sync."
   },
   {
-    "date": "2023-02-16",
+    "date": "2024-02-16",
     "title": "Live Updating Screen for OC Teams",
     "body": "Enhance your conference management with a live updating screen! The Organizing Committee (OC) team can now track countries being claimed and those still available in real-time. Currently available for sponsorship, this feature adds a dynamic layer to your event coordination."
   },
   {
-    "date": "2023-02-17",
+    "date": "2024-02-17",
     "title": "Automated Agenda Suggestions",
     "body": "Allotrix goes beyond allotment! Our platform now suggests engaging and relevant agendas based on current global issues, historical data, and trending topics. Empower your Delegate Affairs team with smart agenda creation tools."
   },
   {
-    "date": "2023-02-18",
+    "date": "2024-02-18",
     "title": "Comprehensive Analytics and Stats",
     "body": "Dive into more detailed analytics and stats! Gain insights into delegate participation, engagement, and committee dynamics. This feature equips your team with valuable data for informed decision-making and future conference planning."
   }
